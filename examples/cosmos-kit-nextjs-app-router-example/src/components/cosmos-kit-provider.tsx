db39c729f72838f3fd59ad8dc9a89da9dc4bbbf8
John Ostrom <jostrom@arculus.co>
2025-04-26 15:40:02 -0700

    Update Example

--- examples/cosmos-kit-nextjs-app-router-example/src/components/cosmos-kit-provider.tsx
+++ examples/cosmos-kit-nextjs-app-router-example/src/components/cosmos-kit-provider.tsx
@@ -3,18 +3,41 @@
 import "@interchain-ui/react/styles";
 import { ChainProvider } from "@cosmos-kit/react";
 import { chains, assets } from "chain-registry";
-import { wallets } from "@cosmos-kit/keplr";
+import { wallets as arculusWallets } from "@cosmos-kit/arculus-mobile";
+import { MainWalletBase } from "@cosmos-kit/core";
+
+// Define WalletConnect Options using Arculus projectId
+const walletConnectOptions = {
+  signClient: {
+    // Use the projectId from ArculusMobileInfo
+    projectId: 'd5235b42fc7273823b6dc3214c822da3',
+    relayUrl: 'wss://relay.walletconnect.com',
+    metadata: {
+      name: 'CosmosKit Example',
+      description: 'CosmosKit App Router Example',
+      url: typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000/',
+      icons: ['https://your-app-icon-url.png'], // Replace with actual icon URL if available
+    },
+    autoConnect: false
+  },
+};
 
 export function CosmosKitProvider({ children }: { children: React.ReactNode }) {
   return (
     <ChainProvider
       chains={chains}
       assetLists={assets}
-      wallets={wallets}
+      wallets={arculusWallets as unknown as MainWalletBase[]}
       logLevel={"DEBUG"}
       throwErrors={false}
+      modalOptions={{
+        mobile: { displayQRCodeEveryTime: true }
+      }}
+      walletConnectOptions={
+        walletConnectOptions
+      }
     >
       {children}
-    </ChainProvider>
+    </ChainProvider >
   );
 }
