632fb6414ed4194a2eae36ce53ecd9951ac0c7a9
John Ostrom <jostrom@arculus.co>
2025-05-03 10:41:14 -0700

    Correct ios deeplink

--- wallets/arculus-mobile/src/wallet-connect/registry.ts
+++ wallets/arculus-mobile/src/wallet-connect/registry.ts
@@ -52,7 +52,7 @@ export const ArculusMobileInfo: Wallet = {
         case 'android':
           return `${plainAppUrl}://wcV2?${encodedWcUrl}#Intent;package=co.arculus.wallet.android;scheme=arculuswc;end;`;
         default:
-          return `${plainAppUrl}://wcV2?${encodedWcUrl}`;
+          return `${plainAppUrl}://wc?${encodedWcUrl}`;
       }
     },
   },
